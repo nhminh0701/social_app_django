5f9303565bfb66a0b96e75e06ecdd90cfcc47b44
minh hoang nguyen <nhminh0701@gmail.com>
2020-09-28 15:30:00 +0700

    Initial commit

---
+++ accounts/tests.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+TESTED_USER_DATA = {
+    "username": "user",
+    "email": "email@email.email",
+    "password": "password"
+}
+
+FAKE_TOKEN = '123abc'
+
+# Create your tests here.
+class RegisterTestCase(TestCase):
+    def test_register_success(self):
+        """
+        Successful Register will generate a user instance with a token
+        """
+        response = self.client.post(reverse("api:register"), TESTED_USER_DATA)
+        self.assertIsNotNone(response.data["token"])
+
+
+class LoginTestCase(TestCase):
+    def test_login_success(self):
+        """
+        Successful Login will generate a user instance with a token
+        """
+        self.client.post(reverse("api:register"), TESTED_USER_DATA)
+        response = self.client.post(reverse("api:login"), {
+            "username": TESTED_USER_DATA["username"],
+            "password": TESTED_USER_DATA["password"]
+        })
+        self.assertIsNotNone(response.data["token"])
+
+class UserAPITestCase(TestCase):
+    def test_get_user_fail(self):
+        """
+        Unauthorized Status 401 resulted from invalid token
+        """
+        response = self.client.get(reverse("api:user"), HTTP_AUTHORIZATION=f"Token {FAKE_TOKEN}")
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_user_success(self):
+        register_response = self.client.post(reverse("api:register"), TESTED_USER_DATA)
+        token = register_response.json()['token']
+
+        response = self.client.get(reverse("api:user"), HTTP_AUTHORIZATION=f"Token {token}")
+        self.assertEqual(response.status_code, 200)
