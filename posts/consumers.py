444bd03ed49d6d70ecef44e7b83f14f9aafb15dd
minh hoang nguyen <nhminh0701@gmail.com>
2020-10-12 17:18:49 +0700

    Async to Sync

--- posts/consumers.py
+++ posts/consumers.py
@@ -1,47 +1,48 @@
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
-from asgiref.sync import sync_to_async
+from channels.generic.websocket import JsonWebsocketConsumer
+from asgiref.sync import async_to_sync
 from knox.auth import TokenAuthentication
 
-def getUserFromToken(token, knoxAuth, consumer):
-    consumer.user = knoxAuth.authenticate_credentials(token.encode('utf-8'))[0]
+def getUserFromToken(token, knoxAuth):
+    return knoxAuth.authenticate_credentials(token.encode('utf-8'))[0]
 
 
-class LobbyConsumer(AsyncJsonWebsocketConsumer):
-    async def connect(self):
+class LobbyConsumer(JsonWebsocketConsumer):
+    def connect(self):
         self.room_group_name = 'lobby'
         self.knoxAuth = TokenAuthentication()
 
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_layer
         )
 
-    async def receive_json(self, content, **kwargs):
-        await sync_to_async(getUserFromToken)(content['token'], self.knoxAuth, self)
+    def receive_json(self, content, **kwargs):
+        user = getUserFromToken(content['token'], self.knoxAuth)
 
-        print(self.user)
-        await self.channel_layer.group_send(
+        print(user)
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': content['type'],
                 'content': content['content'],
+                'user': user.username,
         })
 
 
-    async def post_post(self, event):
+    def post_post(self, event):
         msg = {
             'type': 'POSTED_POST',
             'content': event['content'],
-            'user': self.user.username,
+            'user': event['user'],
         }
 
-        await self.send_json(content=msg)
+        self.send_json(content=msg)
